e747035de2be5d20916bde24d6f86fa1c2939355
causeidm <causeidm@users.noreply.github.com>
2017-02-03 10:40:21 -0800

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
-  restKey: process.env.REST_KEY || '',
   javascriptKey: process.env.JAVASCRIPT_KEY || '',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
